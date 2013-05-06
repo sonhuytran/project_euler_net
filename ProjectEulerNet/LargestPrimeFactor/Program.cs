bfaf8d26d65625b365cafbe501d29e0dc6143638
unknown <mtpham@DSI02.localapteo.com>
2013-05-06 16:46:40 +0200

    003 Largest Prime Number

---
+++ ProjectEulerNet/LargestPrimeFactor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LargestPrimeFactor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			long n = 600851475143;
+			long lastFactor = 2;
+			bool prime = false;
+
+			while (n > 1 && !prime)
+			{
+				prime = true;
+
+				for (long i = 2; i < n / 2; i++)
+				{
+					if (n % i == 0)
+					{
+						prime = false;
+						lastFactor = i;
+						n /= i;
+						break;
+					}
+				}
+			}
+
+			if (prime)
+			{
+				Console.WriteLine(n);
+			}
+		}
+	}
+}
