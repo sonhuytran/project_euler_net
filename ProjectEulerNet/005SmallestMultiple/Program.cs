e45ef41865ffc1d3dffc875d64862f2520248563
unknown <mtpham@DSI02.localapteo.com>
2013-05-06 17:55:48 +0200

    005 created, not finished

---
+++ ProjectEulerNet/005SmallestMultiple/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _005SmallestMultiple
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[,] myArray = new int[11, 11];
+		}
+	}
+}
