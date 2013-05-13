e5539fb1fdf1cd058a72deae37565cad4e6f2996
Son-Huy TRAN <sonhuytran@gmail.com>
2013-05-13 18:05:44 +0200

    SmallestMultiple

--- ProjectEulerNet/005SmallestMultiple/Program.cs
+++ ProjectEulerNet/005SmallestMultiple/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace _005SmallestMultiple
 {
 	class Program
 	{
+		private static int GreatestCommonDiviser(int a, int b)
+		{
+			int temp;
+
+			while (b > 0)
+			{
+				temp = a % b;
+				a = b;
+				b = temp;
+			}
+
+			return a;
+		}
+
+		private static int LeastCommonMultiple(int a, int b)
+		{
+			int gcd = Program.GreatestCommonDiviser(a, b);
+			return a / gcd * b;
+		}
+
 		static void Main(string[] args)
 		{
-			int[,] myArray = new int[11, 11];
+			int lcm = 2;
+
+			for (int i = 3; i <= 20; i++)
+			{
+				lcm = Program.LeastCommonMultiple(lcm, i);
+			}
+
+			Console.WriteLine(lcm);
 		}
 	}
 }
---
+++ ProjectEulerNet/006SumSquareDifference/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _006SumSquareDifference
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = 100;
+			int squareSum = n * n * (n + 1) * (n + 1) / 4; // [n(n+1)/2]^2
+			int sumSquare = n * (n + 1) * (n * 2 + 1) / 6; // [n(n+1)(2n+1)]/6
+
+			Console.WriteLine(squareSum - sumSquare);
+		}
+	}
+}
